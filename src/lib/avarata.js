5bfcb4a8702f6f1ee015a93905a6320a39da2755
Matias Agelvis <Magelvisdoz@gmail.com>
2022-04-29 16:59:11 -0400

    Improve text function tolerance to capitalization
    Now `Avatara.prototype.text` searches
    the font name in the values of Fonts

--- src/lib/avarata.js
+++ src/lib/avarata.js
@@ -3,23 +3,27 @@ const { createCanvas, registerFont } = require("canvas");
 const Fonts = Object.freeze({
     PLEX: "plex",
     COURIER: "courier",
-    COURSINE: "cousine",
+    COUSINE: "cousine",
     PT: "pt",
     ROBOTO: "roboto",
 });
 
-registerFont("fonts/IBMPlexMono-Bold.ttf", { family: Fonts.PLEX });
-registerFont("fonts/CourierPrime-Bold.ttf", { family: Fonts.COURIER });
-registerFont("fonts/Cousine-Bold.ttf", { family: Fonts.COURSINE });
-registerFont("fonts/PTMono-Regular.ttf", { family: Fonts.PT });
-registerFont("fonts/RobotoMono-Bold.ttf", { family: Fonts.ROBOTO });
+registerFont("./fonts/IBMPlexMono-Bold.ttf", { family: Fonts.PLEX });
+registerFont("./fonts/CourierPrime-Bold.ttf", { family: Fonts.COURIER });
+registerFont("./fonts/Cousine-Bold.ttf", { family: Fonts.COUSINE });
+registerFont("./fonts/PTMono-Regular.ttf", { family: Fonts.PT });
+registerFont("./fonts/RobotoMono-Bold.ttf", { family: Fonts.ROBOTO });
 
 const fontFactos = {};
 fontFactos[Fonts.PLEX] = 0.35;
 fontFactos[Fonts.COURIER] = 0.36;
-fontFactos[Fonts.COURSINE] = 0.35;
+fontFactos[Fonts.COUSINE] = 0.35;
 fontFactos[Fonts.PT] = 0.35;
 fontFactos[Fonts.ROBOTO] = 0.35;
+
+function getKeyByValue(object, value) {
+    return Object.keys(object).find((key) => object[key] === value);
+}
 
 function Avatara(width = 200, height = 200) {
     this.canvas = createCanvas(width, height);
@@ -51,6 +55,17 @@ Avatara.prototype.rectangle = function (color = "000") {
     return this;
 };
 
+Avatara.prototype.square = function (color = "000") {
+    this.ctx.fillStyle = `#${color}`;
+    this.ctx.fillRect(
+        0.13 * this.canvas.width,
+        0.13 * this.canvas.height,
+        0.76 * this.canvas.width,
+        0.76 * this.canvas.height
+    );
+    return this;
+};
+
 Avatara.prototype.circle = function (color) {
     this.ctx.arc(
         this.canvas.width / 2,
@@ -67,7 +82,7 @@ Avatara.prototype.circle = function (color) {
 
 Avatara.prototype.gradient = function (back = "000", fore = "fff") {
     // Create a linear gradient
-    var gradient = ctx.createLinearGradient(
+    var gradient = this.ctx.createLinearGradient(
         0,
         0,
         this.canvas.width,
@@ -79,18 +94,17 @@ Avatara.prototype.gradient = function (back = "000", fore = "fff") {
     gradient.addColorStop(1, `#${fore}`);
 
     // Set the fill style and draw a rectangle
-    ctx.fillStyle = gradient;
-    ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.fillStyle = gradient;
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
     return this;
 };
 
 Avatara.prototype.text = function (text = "", color = "fff", font = "Plex") {
-    let fontName = font.toUpperCase();
+    let fontName = getKeyByValue(Fonts, font.toLowerCase());
+    let fontFacor = fontFactos[Fonts[fontName]] * this.canvas.width;
 
     this.ctx.fillStyle = `#${color}`;
-    this.ctx.font = `bold ${
-        fontFactos[Fonts[fontName]] * this.canvas.width
-    }px ${Fonts[fontName]}`;
+    this.ctx.font = `bold ${fontFacor}px ${Fonts[fontName]}`;
     this.ctx.textAlign = "center";
     this.ctx.textBaseline = "middle";
     this.ctx.fillText(
