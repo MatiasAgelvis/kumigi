edf5cec26e4577bb6384465cec7b8123cff48ac1
Matias Agelvis <Magelvisdoz@gmail.com>
2022-05-01 20:34:54 -0400

    Feature add `/compose` route
    The `/compose` route accepts all shapes that can be created normally,
    it will apply them in the same order, colors are passed as an array in
    the same order as the shapes that they will be applied to.

--- src/back/server.js
+++ src/back/server.js
@@ -8,95 +8,68 @@ const app = express();
 
 app.use(compression());
 app.use(helmet());
-app.use(constructor);
 
-function constructor(req, res, next) {
-    const { height = 200, width = 200 } = req.query;
-    res.locals.avatar = new Avatara(width, height);
-    return next();
-}
+app.get("/favicon.ico", (req, res) => res.status(204));
 
-app.get("/", constructor, (req, res) => {
-    const { avatar } = res.locals;
-
+app.get("/", (req, res) => {
     res.json({
         paths: ["basic", "rectangle", "circle", "gradient"],
         colors: ["back", "fore", "textColor"],
         text: ["string"],
-        fonts: avatar.fonts(),
+        fonts: new Avatara().fonts(),
     });
 });
 
-app.get("/basic", constructor, (req, res) => {
-    const { avatar } = res.locals;
+function circular(X, i) {
+    return X[i % X.length];
+}
+
+function parseToArrayOfStrings(arrayString){
+    return arrayString.replace(/\[|\]/g,'').split(',')
+}
+
+app.get("/compose/*", (req, res) => {
+    // first two are '' and 'compose'
+    const shapes = req.path.split("/").slice(2);
 
     const {
-        back = "000",
+        height = 200,
+        width = 200,
+        colors: colorsString = "[#000]",
         text = "",
-        textColor = "fff",
+        textColor = "#fff",
         font = "Plex",
     } = req.query;
 
-    return res.send(
-        avatar.background(back).text(text, textColor, font).toHTML()
-    );
+    // make array of colors form query
+    const colors = parseToArrayOfStrings(colorsString);
+
+    let avatar = new Avatara(width, height);
+
+    shapes.forEach((shape, i) => {
+        avatar[shape](circular(colors, i));
+    });
+
+    return res.send(avatar.toHTML());
 });
 
-app.get("/rectangle", constructor, (req, res) => {
-    const { avatar } = res.locals;
-
+app.get("/:shape", (req, res) => {
     const {
-        back = "000",
-        fore = "555",
+        height = 200,
+        width = 200,
+        back = "#000",
+        fore = "#999",
         text = "",
-        textColor = "fff",
+        textColor = "#fff",
         font = "Plex",
     } = req.query;
+
+    let avatar = new Avatara(width, height);
 
     return res.send(
         avatar
             .background(back)
-            .rectangle(fore)
-            .text(text, textColor, font)
-            .toHTML()
-    );
-});
-
-app.get("/circle", constructor, (req, res) => {
-    const { avatar } = res.locals;
-
-    const {
-        back = "000",
-        fore = "555",
-        text = "",
-        textColor = "fff",
-        font = "Plex",
-    } = req.query;
-
-    return res.send(
-        avatar
-            .background(back)
-            .circle(fore)
-            .text(text, textColor, font)
-            .toHTML()
-    );
-});
-
-app.get("/gradient", constructor, (req, res) => {
-    const { avatar } = res.locals;
-
-    const {
-        back = "000",
-        fore = "555",
-        text = "",
-        textColor = "fff",
-        font = "Plex",
-    } = req.query;
-
-    return res.send(
-        avatar
-            .background(back)
-            .gradient(back, fore)
+            [req.params.shape](fore)
             .text(text, textColor, font)
             .toHTML()
     );
