c446daf62211041082dd8da16bb956d5a235ea72
Matias Agelvis <Magelvisdoz@gmail.com>
2022-05-02 22:07:30 -0400

    Niger Share

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,29 +1,48 @@
 import Avatara from "../lib/avatara";
 import { useState, useEffect } from "react";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+import { HexColorPicker } from "react-colorful";
 
 const avatar = new Avatara();
 
 function App() {
+  const options = [
+    { value: "background", label: "Background" },
+    { value: "square", label: "Square" },
+    { value: "circle", label: "Circle" },
+    { value: "rectangle", label: "Rectangle" },
+    { value: "gradient", label: "Gradient" },
+  ];
+  const defaultOption = options[0];
 
-  const [ shape, setShape ] = useState("");
-  const [ color, setColor ] = useState("");
-  const [ image, setImage ] = useState(avatar.toDataURL());
+  const [shape, setShape] = useState(defaultOption);
+  const [color, setColor] = useState("#fff");
+  const [image, setImage] = useState(avatar.toDataURL());
 
-  const onChange = (setter) => (event) => setter(event.target.value)
+  const onChange = (setter) => (event) => setter(event.target.value);
 
   const updateShape = () => {
-    avatar[shape](color)
+    let shape_ = shape['value'] 
+    console.log(shape)
+    avatar[shape_](color);
     setImage(avatar.toDataURL());
     setShape("");
     setColor("");
   };
 
+
   return (
     <div>
       <div>Enter Shape</div>
-      <input type="text" onChange={onChange(setShape)} value={shape} />
+      <Dropdown
+        options={options}
+        onChange={onChange(setShape)}
+        value={defaultOption}
+        placeholder="Select an option"
+      />
       <div>Enter Color</div>
-      <input type="text" onChange={onChange(setColor)} value={color}/>
+      <HexColorPicker color={color} onChange={setColor} />
       <br />
       <button onClick={updateShape}>Enter</button>
       <br />
