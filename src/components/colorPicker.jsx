8ea2a213206f0b4ccfd8a483a70dc3ca6953147d
Matias Agelvis <Magelvisdoz@gmail.com>
2022-05-16 16:39:43 -0400

    Fix unresponsive hex text color input

--- src/components/colorPicker.jsx
+++ src/components/colorPicker.jsx
@@ -8,6 +8,7 @@ import {
 } from "@chakra-ui/react";
 import { RgbaStringColorPicker, HexColorInput } from "react-colorful";
 import colorString from "color-string";
+import { useEffect, useState } from "react";
 
 function rgba2hex(color) {
   return colorString.to.hex(colorString.get.rgb(color));
@@ -18,6 +19,15 @@ function hex2rgba(color) {
 }
 
 function ColorPicker({ color, setColor }) {
+  const [hexPicker, setHexPicker] = useState(rgba2hex(color));
+
+  useEffect(() => {
+    // if the current input is a valid color update color
+    if (colorString.get(hexPicker)) {
+      setColor(hex2rgba(hexPicker));
+    }
+  }, [hexPicker]);
+
   return (
     <Popover>
       <HStack>
@@ -25,12 +35,8 @@ function ColorPicker({ color, setColor }) {
           <Button bg={color} border="1px" borderColor="gray.200" />
         </PopoverTrigger>
         <Input
-          value={rgba2hex(color)}
-          onChange={(e) => {
-            if (colorString.get(e.target.value)) {
-              setColor(hex2rgba(e.target.value));
-            }
-          }}
+          value={hexPicker}
+          onChange={(e) => setHexPicker(e.target.value)}
         />
       </HStack>
 
