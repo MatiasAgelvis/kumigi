99334a98ea80a0a8cd9076ea981cdd69bc3ad4ab
Matias Agelvis <Magelvisdoz@gmail.com>
2022-05-15 21:02:18 -0400

    Add backend common methods to a single file

---
+++ src/back/common.js
@@ -0,0 +1,31 @@
+function circular(X, i) {
+    return X[i % X.length];
+}
+
+function parseArrayString(arrayString, callback = (x) => x) {
+    return (
+        arrayString
+            // remove brackets
+            .replace(/\[|\]/g, "")
+            // replace , not inside parenthesis with |
+            .replace(/,(?![^(]*\))/g, "|")
+            // split by |
+            .split("|")
+            // ensure that all characters are lowercase
+            .map(callback)
+    );
+}
+
+function applyLayers(avatar, layers) {
+    for (let [key, layer] of Object.entries(layers)) {
+        if (layer.shape) {
+            if (layer.shape != "text") {
+                avatar[layer.shape](layer.color);
+            } else {
+                avatar[layer.shape](layer.text, layer.color, layer.font);
+            }
+        }
+    }
+}
+
+module.exports = {circular, parseArrayString, applyLayers}
