e9b7fa1e24513d241f84f2ab6851d88784741419
Matias Agelvis <Magelvisdoz@gmail.com>
2022-06-04 13:06:59 -0400

    Fix footer overflowing the width of the screen

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,13 @@
 import { Flex, Box, Heading, Text, Link } from "@chakra-ui/react";
-import APIurl from './apiUrl'
-import boxOptions from '../utils/boxOptions'
+import APIurl from "./apiUrl";
+import boxOptions from "../utils/boxOptions";
 
-const Footer = ({url}) => {
+const Footer = ({ url }) => {
 	return (
 		<Box {...boxOptions} m="1rem">
 			<Flex w="100%" alignItems="center" justifyContent="space-between">
-				<Text>
-					<APIurl url={url}/>
+				<Text wordBreak="break-all">
+					<APIurl url={url} />
 				</Text>
 			</Flex>
 		</Box>
