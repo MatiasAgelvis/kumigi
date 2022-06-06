748cc8872788a0bd5daacc0ed675420cffdb1724
Matias Agelvis <Magelvisdoz@gmail.com>
2022-06-06 13:25:19 -0400

    Change layout on sm screens

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,20 +4,27 @@ import boxOptions from "../utils/boxOptions";
 const Header = () => {
 	return (
 		<Box {...boxOptions} m="1rem">
-			<Flex w="100%" alignItems="center" justifyContent="space-between">
-				<Heading as="h1">Avatara</Heading>
-				<Text>
-					by
+			<Flex
+				w="100%"
+				alignItems="center"
+				justifyContent="space-between"
+				wrap="wrap"
+			>
+				<Heading as="h1" mr={4}>
+					Avatara
+				</Heading>
+				<Flex ml="auto" direction={["row", "column"]}>
+					<Text mr={1}>by</Text>
 					<Link href="https://matiasagelvis.com">
 						<Heading
 							as="h4"
-							size="md"
-							w={["min-content", "fit-content"]}
+							size={["sm", "md"]}
+							w={["max-content"]}
 						>
 							Matias Agelvis
 						</Heading>
 					</Link>
-				</Text>
+				</Flex>
 			</Flex>
 		</Box>
 	);
