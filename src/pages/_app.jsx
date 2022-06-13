02a767d22c9c256562c23c4bd3f9666e468f1c4f
Matias Agelvis <Magelvisdoz@gmail.com>
2022-06-13 18:38:04 -0400

    Add grab cursor to drag handles

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import "../../public/css/fonts.css";
+import "../../public/css/dragHandle.css";
 
 function App({ Component, pageProps }) {
   return (
