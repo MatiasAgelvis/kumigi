c007f87dcbc56b833f1a802186cc3ba49618952a
Matias Agelvis <Magelvisdoz@gmail.com>
2022-06-13 20:55:37 -0400

    Add squircle, triangle, diamond and radial shapes

--- src/lib/avatara.js
+++ src/lib/avatara.js
@@ -68,13 +68,81 @@ Avatara.prototype.rectangle = function (color = "#000") {
 };
 
 Avatara.prototype.square = function (color = "#000") {
+    const margin = 0.13;
     this.ctx.fillStyle = Color(color).rgb().string();
     this.ctx.fillRect(
-        0.13 * this.canvas.width,
-        0.13 * this.canvas.height,
-        0.76 * this.canvas.width,
-        0.76 * this.canvas.height
+        margin * this.canvas.width,
+        margin * this.canvas.height,
+        (1 - 2 * margin) * this.canvas.width,
+        (1 - 2 * margin) * this.canvas.height
     );
+    return this;
+};
+
+Avatara.prototype.squircle = function (color = "#000") {
+    const segments = 128;
+
+    const PI = Math.PI,
+        TAU = PI * 2,
+        n = 4,
+        step = TAU / segments,
+        dimx = this.canvas.width / 2,
+        dimy = this.canvas.height / 2,
+        r = 0.8,
+        rx = dimx * r,
+        ry = dimy * r;
+
+    function coord(t) {
+        let power = 2 / n;
+        let c = Math.cos(t),
+            x = Math.pow(Math.abs(c), power) * Math.sign(c);
+        let s = Math.sin(t),
+            y = Math.pow(Math.abs(s), power) * Math.sign(s);
+        return { x, y };
+    }
+
+    function drawSegmentTo(t) {
+        let c = coord(t);
+        let cx = dimx + rx * c.x;
+        let cy = dimy + ry * c.y;
+        return { cx, cy };
+    }
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(dimx + rx, dimy);
+
+    for (let t = step; t <= TAU; t += step) {
+        let { cx, cy } = drawSegmentTo(t);
+        this.ctx.lineTo(cx, cy);
+    }
+
+    this.ctx.moveTo(dimx + rx, dimy);
+    this.ctx.closePath();
+
+    this.ctx.fillStyle = Color(color).rgb().string();
+    this.ctx.fill();
+
+    return this;
+};
+
+Avatara.prototype.triangle = function (color = "#000") {
+    this.ctx.fillStyle = Color(color).rgb().string();
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.canvas.width / 2, 0);
+    this.ctx.lineTo(0, this.canvas.height);
+    this.ctx.lineTo(this.canvas.width, this.canvas.height);
+    this.ctx.fill();
+    return this;
+};
+
+Avatara.prototype.diamond = function (color = "#000") {
+    this.ctx.fillStyle = Color(color).rgb().string();
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.canvas.width / 2, 0);
+    this.ctx.lineTo(this.canvas.width, this.canvas.height / 2);
+    this.ctx.lineTo(this.canvas.width / 2, this.canvas.height);
+    this.ctx.lineTo(0, this.canvas.height / 2);
+    this.ctx.fill();
     return this;
 };
 
@@ -82,7 +150,7 @@ Avatara.prototype.circle = function (color = "#777") {
     this.ctx.arc(
         this.canvas.width / 2,
         this.canvas.height / 2,
-        this.canvas.height * 0.45,
+        Math.min(this.canvas.width, this.canvas.height) * 0.45,
         0,
         2 * Math.PI
     );
@@ -92,7 +160,7 @@ Avatara.prototype.circle = function (color = "#777") {
     return this;
 };
 
-Avatara.prototype.gradient = function (color = "#00f") {
+Avatara.prototype.linear = function (color = "#00f") {
     // Create a linear gradient
     var gradient = this.ctx.createLinearGradient(
         0,
@@ -104,6 +172,27 @@ Avatara.prototype.gradient = function (color = "#00f") {
     // Add three color stops
     gradient.addColorStop(0, Color(color).alpha(0).rgb().string());
     gradient.addColorStop(1, Color(color).rgb().string());
+
+    // Set the fill style and draw a rectangle
+    this.ctx.fillStyle = gradient;
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    return this;
+};
+
+Avatara.prototype.radial = function (color = "#00f") {
+    // Create a linear gradient
+    var gradient = this.ctx.createRadialGradient(
+        this.canvas.width / 2,
+        this.canvas.height / 2,
+        0,
+        this.canvas.width / 2,
+        this.canvas.height / 2,
+        Math.min(this.canvas.width, this.canvas.height)
+    );
+
+    // Add three color stops
+    gradient.addColorStop(0, Color(color).rgb().string());
+    gradient.addColorStop(1, Color(color).alpha(0).rgb().string());
 
     // Set the fill style and draw a rectangle
     this.ctx.fillStyle = gradient;
