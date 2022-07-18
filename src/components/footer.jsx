74050cdfca590fa894e5437da066b6774a34875a
Matias Agelvis <Magelvisdoz@gmail.com>
2022-07-18 11:16:14 -0400

    Add self-promotion to bottom

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,15 +1,35 @@
-import { Flex, Box, Heading, Text, Link } from "@chakra-ui/react";
+import {
+	Flex,
+	Box,
+	Heading,
+	Text,
+	Link,
+	VStack,
+	Center,
+} from "@chakra-ui/react";
 import APIurl from "./apiUrl";
 import boxOptions from "../utils/boxOptions";
 
 const Footer = ({ url }) => {
 	return (
-		<Box {...boxOptions} m="1rem">
-			<Flex w="100%" alignItems="center" justifyContent="space-between">
-				<Text wordBreak="break-all">
-					<APIurl url={url} />
-				</Text>
-			</Flex>
+		<Box>
+			<Box {...boxOptions} m="1rem">
+				<Flex
+					w="100%"
+					alignItems="center"
+					justifyContent="space-between"
+				>
+					<Text wordBreak="break-all">
+						<APIurl url={url} />
+					</Text>
+				</Flex>
+			</Box>
+			<Center>
+				<Text mr={2}>by</Text>
+				<Heading size={"lg"}>
+					<Link href="https://matiasagelvis.com">Matias Agelvis</Link>
+				</Heading>
+			</Center>
 		</Box>
 	);
 };
