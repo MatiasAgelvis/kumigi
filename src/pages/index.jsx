757bc96a4ff9aa25e123906382408ba38c8f5cac
Matias Agelvis <Magelvisdoz@gmail.com>
2022-07-20 17:58:32 -0400

    Remove unnecessary GridItem’s and props

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,14 @@
 import { Grid, GridItem } from "@chakra-ui/react";
-import { useState } from "react";
 import App from "../components/app";
 import Footer from "../components/footer";
 import Header from "../components/header";
-import { BASE } from "../utils/url";
 
 function Main() {
-  const [url, setURL] = useState(BASE);
-
   return (
     <Grid templateRows="repeat(3, auto)">
-      <GridItem>
-        <Header />
-      </GridItem>
-      <GridItem>
-        <App setURL={setURL} />
-      </GridItem>
-      <GridItem>
-        <Footer url={url} />
-      </GridItem>
+      <Header />
+      <App />
+      <Footer />
     </Grid>
   );
 }
