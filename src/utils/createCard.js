cef615b28df7a91182e669282323fd70caf3ceca
Matias Agelvis <Magelvisdoz@gmail.com>
2022-07-21 16:30:48 -0400

    Add idCard function

--- src/utils/createCard.js
+++ src/utils/createCard.js
@@ -4,4 +4,8 @@ export function createCard() {
   return { id: uuidv4(), shape: null, color: "rgba(0,0,0,1)" };
 }
 
+export function idCard(layer) {
+  return { id: uuidv4(), ...layer };
+}
+
 export const layers__Default = [createCard()];
