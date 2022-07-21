ba5c8a00260f0571a70115af861a5c92410def22
Matias Agelvis <Magelvisdoz@gmail.com>
2022-07-21 16:49:03 -0400

    Add randomize button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,31 +1,49 @@
-import { Flex, Box, Heading, HStack, Button } from "@chakra-ui/react";
+import {
+	Flex,
+	Box,
+	Heading,
+	HStack,
+	Button,
+	useBreakpointValue,
+	Wrap,
+	SimpleGrid,
+} from "@chakra-ui/react";
 import { useRecoilState } from "recoil";
 import boxOptions from "../utils/boxOptions";
 import ColorToggle from "./colorToggle";
 import { layersAtom } from "../utils/store";
-import { layers__Default } from "../utils/createCard";
+import Avatara from "../lib/avatara";
+import { idCard } from "../utils/createCard";
 
 const Header = () => {
 	const [layers, setLayers] = useRecoilState(layersAtom);
 	return (
-		<Box {...boxOptions} m="1rem">
-			<Flex
-				w="100%"
-				alignItems="center"
-				justifyContent="space-between"
-				wrap="wrap"
-			>
-				<Heading as="h1" mr={4}>
-					Avatara
-				</Heading>
-				<HStack spacing={4}>
-					<Button colorScheme="red" onClick={() => setLayers([])}>
-						Reset
-					</Button>
-					<ColorToggle />
-				</HStack>
-			</Flex>
-		</Box>
+		<SimpleGrid columns={[1, 2]} {...boxOptions} m="1rem">
+			<Heading as="h1" mr={4} mb={4}>
+				Avatara
+			</Heading>
+			<Wrap spacing={4} justify="end">
+				<Button
+					size={useBreakpointValue({ base: "sm", md: "md" })}
+					colorScheme="blue"
+					onClick={() =>
+						setLayers(
+							new Avatara().randomLayers().map((layer) => idCard(layer))
+						)
+					}
+				>
+					Randomize
+				</Button>
+				<Button
+					size={useBreakpointValue({ base: "sm", md: "md" })}
+					colorScheme="red"
+					onClick={() => setLayers([])}
+				>
+					Reset
+				</Button>
+				<ColorToggle size={useBreakpointValue({ base: "sm", md: "md" })} />
+			</Wrap>
+		</SimpleGrid>
 	);
 };
 
