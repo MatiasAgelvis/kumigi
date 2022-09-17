9ebed45c7bf1375ca87243cd120725bbb2235ac0
Matias Agelvis <Magelvisdoz@gmail.com>
2022-09-17 19:29:21 -0400

    Create new Next Api endpont

---
+++ src/pages/api/[...shapes].js
@@ -0,0 +1,23 @@
+import Avatara from '../../lib/avatara'
+import applyLayers from '../../utils/applyLayers'
+import { parseAvataraQuery } from '../../utils/parseAvataraQuery';
+
+const avatar = (req, res) => {
+
+  const [height, width, layers] = parseAvataraQuery(req.query, req.query.shapes || []);
+
+  const avatar = new Avatara(width, height);
+  applyLayers(avatar, layers);
+
+  res.statusCode = 200;
+    
+  // returns the image directly to be downloaded
+  res.setHeader('Content-Type', 'image/png')
+  return res.send(avatar.toBuffer());
+
+  // return an html page just with the image
+  // res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  // return res.send(avatar.toHTML());
+};
+
+export default avatar;
