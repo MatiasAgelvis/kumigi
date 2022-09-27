338e15e812150d0e4aabcaf7fce430486fd7fa62
Matias Agelvis <Magelvisdoz@gmail.com>
2022-09-26 21:00:36 -0400

    Add background pattern to imageBox

--- app/components/designer/image/imageBox.jsx
+++ app/components/designer/image/imageBox.jsx
@@ -8,6 +8,9 @@ function ImageBox({ image }) {
       border="2px"
       borderColor="gray.200"
       _dark={{ borderColor: "gray.600" }}
+      background={
+        "repeating-conic-gradient(#808080 0% 25%, transparent 0% 50%) 50% / 20px 20px"
+      }
       shadow="md"
       fit="scale-down"
       maxH="70vh"
