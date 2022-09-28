a2a477a30d60315fcfcfa5c4074b0446ba34c6d5
Matias Agelvis <Magelvisdoz@gmail.com>
2022-09-27 21:50:08 -0400

    Use new Avatara grapheme splitter

--- app/components/designer/card/textInput.jsx
+++ app/components/designer/card/textInput.jsx
@@ -1,21 +1,29 @@
 // import Select from "react-select";
-import { Input, InputGroup, InputRightAddon, Code } from "@chakra-ui/react";
-import GraphemeSplitter from "grapheme-splitter";
+import {
+  Input,
+  InputGroup,
+  InputRightAddon,
+  Code,
+  Highlight,
+  Text,
+  Box,
+} from "@chakra-ui/react";
+import { textLength } from "app/lib/avatara";
 
 function TextInput({ text, setText, maxLength = 3 }) {
-  const splitter = new GraphemeSplitter();
   return (
     <InputGroup>
       <Input
         value={text}
         onChange={(event) => setText(event.target.value)}
-        maxLength={maxLength}
+        // maxLength={maxLength}
         placeholder="Input your text"
       />
       <InputRightAddon
+        {...(textLength(text) > maxLength && { backgroundColor: "red.200" })}
         children={
-          <Code>
-            {splitter.splitGraphemes(text).length}/{maxLength}
+          <Code background={"transparent"}>
+            {textLength(text)}/{maxLength}
           </Code>
         }
       />
