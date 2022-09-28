b8526d09d4fbe99e49902e2a2e7fed10bb720b22
Matias Agelvis <Magelvisdoz@gmail.com>
2022-09-28 16:27:13 -0400

    Fix apiUrl lineBreak

--- app/components/footer/apiUrl.jsx
+++ app/components/footer/apiUrl.jsx
@@ -1,5 +1,11 @@
 import { Link } from "@chakra-ui/react";
 
 export default function APIurl({ url }) {
-  return url && <Link href={url}>{url}</Link>;
+  return (
+    url && (
+      <Link style={{ lineBreak: "anywhere" }} href={url}>
+        {url}
+      </Link>
+    )
+  );
 }
