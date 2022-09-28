025e65b60946caed068cb01ed3508e52b50ba519
Matias Agelvis <Magelvisdoz@gmail.com>
2022-09-27 20:23:54 -0400

    Refactor Avatara shapes

---
+++ app/lib/avatara.ts
@@ -0,0 +1,150 @@
+import { createCanvas } from "canvas";
+import Color from "color";
+import {
+  isNode, //isBrowser, isWebWorker, isJsDom, isDeno,
+} from "browser-or-node";
+import GraphemeSplitter from "grapheme-splitter";
+import emoji from "node-emoji";
+import shapes, { Layer, Shape } from "./shapes";
+
+export const Fonts = Object.freeze({
+  PLEX: "plex",
+  COURIER: "courier",
+  COUSINE: "cousine",
+  PT: "pt",
+  ROBOTO: "roboto",
+  EMOJI: "emoji",
+});
+
+if (isNode) {
+  import("canvas").then(({ registerFont }) => {
+    registerFont("public/fonts/IBMPlexMono-Bold.ttf", {
+      family: Fonts.PLEX,
+    });
+    registerFont("public/fonts/CourierPrime-Bold.ttf", {
+      family: Fonts.COURIER,
+    });
+    registerFont("public/fonts/Cousine-Bold.ttf", {
+      family: Fonts.COUSINE,
+    });
+    registerFont("public/fonts/PTMono-Regular.ttf", { family: Fonts.PT });
+    registerFont("public/fonts/RobotoMono-Bold.ttf", {
+      family: Fonts.ROBOTO,
+    });
+    registerFont("public/fonts/NotoEmoji-Bold.ttf", {
+      family: Fonts.EMOJI,
+    });
+  });
+}
+
+const FONT_FACTOR = 0.35;
+
+function getKeyByValue(object, value) {
+  return Object.keys(object).find((key) => object[key] === value);
+}
+
+function Avatara(width = 200, height = 200) {
+  this.canvas = createCanvas(width, height);
+  this.ctx = this.canvas.getContext("2d");
+}
+
+Avatara.prototype.fonts = function () {
+  return Object.values(Fonts);
+};
+
+Avatara.prototype.shapes = function () {
+  return [...Object.keys(shapes), "text"];
+};
+
+Object.entries(shapes).map(([shape, func]) => {
+  Avatara.prototype[shape] = function (layer: Layer) {
+    func(this.canvas, this.ctx, layer);
+  };
+});
+
+Avatara.prototype.text = async function ({
+  color = "#fff",
+  text = "",
+  font = "plex",
+}) {
+  const splitter = new GraphemeSplitter();
+  const fontName = getKeyByValue(Fonts, font.toLowerCase());
+  const fontFactor = 3 * FONT_FACTOR * this.canvas.width;
+  let str = splitter
+    .splitGraphemes(emoji.emojify(text))
+    .slice(0, 3)
+    .join("")
+    .normalize();
+
+  this.ctx.fillStyle = Color(color).rgb().string();
+
+  this.ctx.font = `bold ${fontFactor / str.length}px ${Fonts[fontName]}, ${
+    Fonts.EMOJI
+  }`;
+  this.ctx.textAlign = "center";
+  this.ctx.textBaseline = "middle";
+  this.ctx.fillText(str, this.canvas.width / 2, this.canvas.height / 2);
+
+  return this;
+};
+
+Avatara.prototype.toHTML = function () {
+  return `<img src=" ${this.canvas.toDataURL()} " />`;
+};
+
+Avatara.prototype.toDataURL = function () {
+  return this.canvas.toDataURL();
+};
+
+Avatara.prototype.toBuffer = function () {
+  return this.canvas.toBuffer();
+};
+
+function choose(choices) {
+  const index = Math.floor(Math.random() * choices.length);
+  return choices[index];
+}
+
+function randomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function randomText() {
+  const size = Math.round(Math.random() * 2) + 1;
+  let letters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789,.@#$%^&*(){}[]|/";
+  var text = "";
+  for (var i = 0; i < size; i++) {
+    text += choose(letters);
+  }
+  return text;
+}
+
+export function randomLayer() {
+  const shape = choose(Object.keys(shapes));
+  return {
+    shape: shape,
+    color: randomColor(),
+    ...(shape == "text" && { text: randomText(), font: choose(Fonts) }),
+  };
+}
+
+export function randomLayers(min = 1, max = 5) {
+  let numLayers = Math.ceil(Math.random() * max) + min;
+  return Array.from(Array(numLayers)).map((_) => randomLayer());
+}
+
+Avatara.prototype.randomAvatar = function () {
+  let layers = randomLayers();
+  layers.forEach((layer) => {
+    this[layer.shape](layer);
+  });
+  return this;
+};
+
+export default Avatara;
---
+++ app/lib/shapes.ts
@@ -0,0 +1,180 @@
+import { Canvas } from "canvas";
+import Color from "color";
+
+export type Shape = keyof typeof shapes;
+export type Layer = {
+  shape: Shape;
+  color: string;
+  text?: string;
+  font?: string;
+};
+
+const shapes = {
+  background: function (
+    canvas: Canvas,
+    ctx: CanvasRenderingContext2D,
+    { color = "#000" }: Layer
+  ) {
+    ctx.fillStyle = Color(color).rgb().string();
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  },
+  rectangle: function (
+    canvas: Canvas,
+    ctx: CanvasRenderingContext2D,
+    { color = "#000" }: Layer
+  ) {
+    ctx.fillStyle = Color(color).rgb().string();
+    ctx.fillRect(0, 0.2 * canvas.height, canvas.width, 0.6 * canvas.height);
+  },
+  square: function (
+    canvas: Canvas,
+    ctx: CanvasRenderingContext2D,
+    { color = "#000" }: Layer
+  ) {
+    const margin = 0.13;
+    ctx.fillStyle = Color(color).rgb().string();
+    ctx.fillRect(
+      margin * canvas.width,
+      margin * canvas.height,
+      (1 - 2 * margin) * canvas.width,
+      (1 - 2 * margin) * canvas.height
+    );
+  },
+  squircle: function (
+    canvas: Canvas,
+    ctx: CanvasRenderingContext2D,
+    { color = "#000" }: Layer
+  ) {
+    const segments = 128;
+
+    const PI = Math.PI,
+      TAU = PI * 2,
+      n = 4,
+      step = TAU / segments,
+      dimx = canvas.width / 2,
+      dimy = canvas.height / 2,
+      r = 0.8,
+      rx = dimx * r,
+      ry = dimy * r;
+
+    function coord(t: number) {
+      let power = 2 / n;
+      let c = Math.cos(t),
+        x = Math.pow(Math.abs(c), power) * Math.sign(c);
+      let s = Math.sin(t),
+        y = Math.pow(Math.abs(s), power) * Math.sign(s);
+      return { x, y };
+    }
+
+    function drawSegmentTo(t: number) {
+      let c = coord(t);
+      let cx = dimx + rx * c.x;
+      let cy = dimy + ry * c.y;
+      return { cx, cy };
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(dimx + rx, dimy);
+
+    for (let t = step; t <= TAU; t += step) {
+      let { cx, cy } = drawSegmentTo(t);
+      ctx.lineTo(cx, cy);
+    }
+
+    ctx.moveTo(dimx + rx, dimy);
+    ctx.closePath();
+
+    ctx.fillStyle = Color(color).rgb().string();
+    ctx.fill();
+  },
+  triangle: function (
+    canvas: Canvas,
+    ctx: CanvasRenderingContext2D,
+    { color = "#000" }: Layer
+  ) {
+    ctx.fillStyle = Color(color).rgb().string();
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, 0);
+    ctx.lineTo(0, canvas.height);
+    ctx.lineTo(canvas.width, canvas.height);
+    ctx.closePath();
+    ctx.fill();
+  },
+
+  diamond: function (
+    canvas: Canvas,
+    ctx: CanvasRenderingContext2D,
+    { color = "#000" }: Layer
+  ) {
+    ctx.fillStyle = Color(color).rgb().string();
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, 0);
+    ctx.lineTo(canvas.width, canvas.height / 2);
+    ctx.lineTo(canvas.width / 2, canvas.height);
+    ctx.lineTo(0, canvas.height / 2);
+    ctx.closePath();
+    ctx.fill();
+  },
+
+  circle: function (
+    canvas: Canvas,
+    ctx: CanvasRenderingContext2D,
+    { color = "#000" }: Layer
+  ) {
+    ctx.beginPath();
+    ctx.ellipse(
+      canvas.width / 2,
+      canvas.height / 2,
+      canvas.width * 0.45,
+      canvas.height * 0.45,
+      0,
+      0,
+      2 * Math.PI
+    );
+    ctx.closePath();
+    ctx.fillStyle = Color(color).rgb().string();
+    ctx.fill();
+  },
+
+  linear: function (
+    canvas: Canvas,
+    ctx: CanvasRenderingContext2D,
+    { color = "#000" }: Layer
+  ) {
+    // Create a linear gradient
+    var gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+
+    // Add three color stops
+    gradient.addColorStop(0, Color(color).alpha(0).rgb().string());
+    gradient.addColorStop(1, Color(color).rgb().string());
+
+    // Set the fill style and draw a rectangle
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  },
+  radial: function (
+    canvas: Canvas,
+    ctx: CanvasRenderingContext2D,
+    { color = "#000" }: Layer
+  ) {
+    // Create a linear gradient
+    var gradient = ctx.createRadialGradient(
+      canvas.width / 2,
+      canvas.height / 2,
+      0,
+      canvas.width / 2,
+      canvas.height / 2,
+      Math.min(canvas.width, canvas.height)
+    );
+
+    // Add three color stops
+    gradient.addColorStop(0, Color(color).rgb().string());
+    gradient.addColorStop(1, Color(color).alpha(0).rgb().string());
+
+    // Set the fill style and draw a rectangle
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  },
+};
+
+export default shapes;
