93e2ee29c25a8404a835dbcfb76d2ec6102288bb
Matias Agelvis <Magelvisdoz@gmail.com>
2022-09-27 20:24:24 -0400

    Refactor applyLayers.js to new Avatara API

--- app/utils/applyLayers.js
+++ app/utils/applyLayers.js
@@ -1,11 +1,7 @@
 export default function applyLayers(avatar, layers) {
   layers.forEach((layer) => {
     if (layer.shape) {
-      if (layer.shape != "text") {
-        avatar[layer.shape](layer.color);
-      } else {
-        avatar[layer.shape](layer.color, layer.text, layer.font);
-      }
+      avatar[layer.shape](layer);
     }
   });
 }
