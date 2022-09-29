65f9e018bde73bf5651f68f090aa947d7aad0e44
Matias Agelvis <Magelvisdoz@gmail.com>
2022-09-28 22:00:15 -0400

    Add animation LayoutGroup to main component group
    LayoutGroup prevents component teleportation on layer deletes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import { Suspense } from "react";
 import Layout from "app/core/layouts/Layout";
 import { BlitzPage } from "@blitzjs/next";
-import { Grid } from "@chakra-ui/react";
+import { Box, Grid, Stack, VStack } from "@chakra-ui/react";
 import Header from "app/components/header";
 import Footer from "app/components/footer";
 import Designer from "app/components/designer";
 import UserInfo from "app/components/header/UserInfo";
-
+import { AnimatePresence, LayoutGroup, motion } from "framer-motion";
+import animationOptions from "app/utils/animationOptions";
 /*
  * This file is just for a pleasant getting started page for your new app.
  * You can delete everything in here and start from scratch if you like.
@@ -19,12 +20,15 @@ const Home: BlitzPage = () => {
         {/*<Suspense fallback="Loading...">
           <UserInfo />
         </Suspense>*/}
-
-        <Grid templateRows="repeat(3, auto)">
-          <Header />
-          <Designer />
-          <Footer />
-        </Grid>
+        <LayoutGroup>
+          <VStack w="full" alignItems={"stretch"}>
+            <Header />
+            <Designer />
+            <motion.div {...animationOptions}>
+              <Footer />
+            </motion.div>
+          </VStack>
+        </LayoutGroup>
       </main>
     </Layout>
   );
