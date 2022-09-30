5c115fda974de7f95db11b3922c6322e98120013
Matias Agelvis <Magelvisdoz@gmail.com>
2022-09-29 21:07:49 -0400

    Remove random and reset buttons

--- app/components/header/index.jsx
+++ app/components/header/index.jsx
@@ -1,47 +1,30 @@
 import {
+  Box,
   Button,
   Heading,
   SimpleGrid,
   useBreakpointValue,
   Wrap,
 } from "@chakra-ui/react";
+import { Suspense } from "react";
 import { useRecoilState } from "recoil";
 import { randomLayers } from "../../lib/avatara";
 import boxOptions from "../../utils/boxOptions";
 import { createCard, idCard } from "../../utils/createCard";
 import { layersAtom } from "../../utils/store";
 import ColorToggle from "./colorToggle";
+import UserInfo from "./UserInfo";
 
 const Header = () => {
-  const [layers, setLayers] = useRecoilState(layersAtom);
   return (
     <SimpleGrid columns={[1, 2]} {...boxOptions} m="1rem">
       <Heading as="h1" mr={4} mb={[4, 0]}>
         Avatara
       </Heading>
       <Wrap spacing={4} justify="end">
-        <Button
-          size={useBreakpointValue({ base: "sm", md: "md" })}
-          colorScheme="blue"
-          onClick={() =>
-            setLayers(randomLayers().map((layer) => idCard(layer)))
-          }
-        >
-          Randomize
-        </Button>
-        <Button
-          size={useBreakpointValue({ base: "sm", md: "md" })}
-          colorScheme="red"
-          onClick={() => {
-            try {
-              setLayers([createCard()]);
-            } catch (e) {
-              console.log(e);
-            }
-          }}
-        >
-          Reset
-        </Button>
+        <Suspense fallback={<Button disabled>Loading...</Button>}>
+          <UserInfo />
+        </Suspense>
         <ColorToggle size={useBreakpointValue({ base: "sm", md: "md" })} />
       </Wrap>
     </SimpleGrid>
