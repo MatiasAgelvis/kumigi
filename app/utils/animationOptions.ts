971ed288ada17a1fcb8c11335ad3149742029623
Matias Agelvis <Magelvisdoz@gmail.com>
2022-09-30 21:11:07 -0400

    Update animations

--- app/utils/animationOptions.ts
+++ app/utils/animationOptions.ts
@@ -1,10 +1,9 @@
 import { MotionProps } from "framer-motion";
 
 const animationOptions: MotionProps = {
-  layout: true,
   initial: { scale: 0.8, opacity: 0 },
   animate: { scale: 1, opacity: 1 },
-  exit: { scale: 0.8, opacity: 0 },
+  exit: { scale: 0.5, opacity: 0 },
   transition: { type: "spring", duration: 0.8 },
 };
 
