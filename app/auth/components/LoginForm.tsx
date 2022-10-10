134593d930e8655d02f27cc706bf4ed86b9bdca3
Matias Agelvis <Magelvisdoz@gmail.com>
2022-10-10 19:24:46 -0400

    Style LoginForm with Chakra

--- app/auth/components/LoginForm.tsx
+++ app/auth/components/LoginForm.tsx
@@ -1,59 +1,60 @@
-import { AuthenticationError, PromiseReturnType } from "blitz"
-import Link from "next/link"
-import { LabeledTextField } from "app/core/components/LabeledTextField"
-import { Form, FORM_ERROR } from "app/core/components/Form"
-import login from "app/auth/mutations/login"
-import { Login } from "app/auth/validations"
-import { useMutation } from "@blitzjs/rpc"
-import { Routes } from "@blitzjs/next"
+import { AuthenticationError, PromiseReturnType } from "blitz";
+import NextLink from "next/link";
+import { LabeledTextField } from "app/core/components/LabeledTextField";
+import { Form, FORM_ERROR } from "app/core/components/Form";
+import login from "app/auth/mutations/login";
+import { Login } from "app/auth/validations";
+import { useMutation } from "@blitzjs/rpc";
+import { Routes } from "@blitzjs/next";
+import { Box, Center, Link, Text } from "@chakra-ui/react";
+import boxOptions from "app/utils/boxOptions";
 
 type LoginFormProps = {
-  onSuccess?: (user: PromiseReturnType<typeof login>) => void
-}
+  onSuccess?: (user: PromiseReturnType<typeof login>) => void;
+};
 
 export const LoginForm = (props: LoginFormProps) => {
-  const [loginMutation] = useMutation(login)
+  const [loginMutation] = useMutation(login);
   return (
-    <div>
-      <h1>Login</h1>
-
-      <Form
-        submitText="Login"
-        schema={Login}
-        initialValues={{ email: "", password: "" }}
-        onSubmit={async (values) => {
-          try {
-            const user = await loginMutation(values)
-            props.onSuccess?.(user)
-          } catch (error: any) {
-            if (error instanceof AuthenticationError) {
-              return { [FORM_ERROR]: "Sorry, those credentials are invalid" }
-            } else {
-              return {
-                [FORM_ERROR]:
-                  "Sorry, we had an unexpected error. Please try again. - " + error.toString(),
+    <Center>
+      <Box {...boxOptions} w={"fit-content"}>
+        <Form
+          submitText="Login"
+          title="Login"
+          schema={Login}
+          initialValues={{ email: "", password: "" }}
+          onSubmit={async (values) => {
+            try {
+              const user = await loginMutation(values);
+              props.onSuccess?.(user);
+            } catch (error: any) {
+              if (error instanceof AuthenticationError) {
+                return { [FORM_ERROR]: "Sorry, those credentials are invalid" };
+              } else {
+                return {
+                  [FORM_ERROR]:
+                    "Sorry, we had an unexpected error. Please try again. - " +
+                    error.toString(),
+                };
               }
             }
-          }
-        }}
-      >
-        <LabeledTextField name="email" label="Email" placeholder="Email" />
-        <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
-        <div>
-          <Link href={Routes.ForgotPasswordPage()}>
-            <a>Forgot your password?</a>
-          </Link>
-        </div>
-      </Form>
+          }}
+        >
+          <LabeledTextField name="email" label="Email" />
+          <LabeledTextField name="password" label="Password" type="password" />
+          <NextLink href={Routes.ForgotPasswordPage()}>
+            <Link>Forgot your password?</Link>
+          </NextLink>
+        </Form>
+        <Text align={"center"}>
+          Or{" "}
+          <NextLink href={Routes.SignupPage()} passHref>
+            <Link>Sign Up</Link>
+          </NextLink>
+        </Text>
+      </Box>
+    </Center>
+  );
+};
 
-      <div style={{ marginTop: "1rem" }}>
-        Or{" "}
-        <Link href={Routes.SignupPage()}>
-          <a>Sign Up</a>
-        </Link>
-      </div>
-    </div>
-  )
-}
-
-export default LoginForm
+export default LoginForm;
