dd2e8dde5bcabb8bf09e7d38747617899018cb21
Matias Agelvis <Magelvisdoz@gmail.com>
2022-10-10 19:16:36 -0400

    Migrate boxOptions to TS

--- app/utils/boxOptions.js
+++
@@ -1,8 +0,0 @@
-const boxOptions = {
-  borderWidth: "3px",
-  borderRadius: "lg",
-  p: 5,
-  shadow: "md",
-};
-
-export default boxOptions;
---
+++ app/utils/boxOptions.ts
@@ -0,0 +1,14 @@
+import { BoxProps } from "@chakra-ui/react";
+
+const boxOptions: BoxProps = {
+  borderWidth: "3px",
+  borderRadius: "lg",
+  p: 5,
+  shadow: "md",
+  backgroundColor: "whiteAlpha.900",
+  _dark: {
+    backgroundColor: "gray.800",
+  },
+};
+
+export default boxOptions;
