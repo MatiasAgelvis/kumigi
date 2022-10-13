d2efb16d9392d6ea97554e4d0213f9df14685984
Matias Agelvis <Magelvisdoz@gmail.com>
2022-10-12 20:20:30 -0400

    Add link to home in header title

--- app/components/header/index.jsx
+++ app/components/header/index.jsx
@@ -1,3 +1,4 @@
+import { Routes } from "@blitzjs/next";
 import {
   Box,
   Button,
@@ -6,12 +7,9 @@ import {
   useBreakpointValue,
   Wrap,
 } from "@chakra-ui/react";
+import Link from "next/link";
 import { Suspense } from "react";
-import { useRecoilState } from "recoil";
-import { randomLayers } from "../../lib/avatara";
 import boxOptions from "../../utils/boxOptions";
-import { createCard, idCard } from "../../utils/createCard";
-import { layersAtom } from "../../utils/store";
 import ColorToggle from "./colorToggle";
 import UserInfo from "./UserInfo";
 
@@ -19,7 +17,7 @@ const Header = () => {
   return (
     <SimpleGrid columns={[1, 2]} {...boxOptions} m="1rem">
       <Heading as="h1" mr={4} mb={[4, 0]}>
-        Avatara
+        <Link href={Routes.Home()}>Avatara</Link>
       </Heading>
       <Wrap spacing={4} justify="end">
         <Suspense fallback={<Button disabled>Loading...</Button>}>
