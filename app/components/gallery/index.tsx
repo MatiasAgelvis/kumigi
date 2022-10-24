1fc1f4405272dbd191ca32c0e7a1ea04995d2d63
Matias Agelvis <Magelvisdoz@gmail.com>
2022-10-24 17:11:43 -0400

    Route images to editor
    Create avatars with the global height and width parameters

--- app/components/gallery/index.tsx
+++ app/components/gallery/index.tsx
@@ -1,3 +1,4 @@
+import { Routes } from "@blitzjs/next";
 import boxOptions from "app/utils/boxOptions";
 import {
   Box,
@@ -15,26 +16,38 @@ import ImageBox from "../designer/image/imageBox";
 import Avatara, { randomLayers } from "app/lib/avatara";
 import applyLayers from "app/utils/applyLayers";
 import Modalo from "../modal";
+import sizeState from "app/utils/sizeState";
+import { useRecoilState } from "recoil";
+import { layersAtom } from "app/utils/store";
+import { useRouter } from "next/router";
 
 function Dummy() {
-  const avatar = new Avatara();
+  const { height, width } = sizeState();
+  const avatar = new Avatara(width, height);
   const [layers, setLayers] = useState(randomLayers());
   applyLayers(avatar, layers);
+  const [editorLayers, setEditorLayers] = useRecoilState(layersAtom);
+  const router = useRouter();
 
   const imageBox = (
     <Center>
       <ImageBox image={avatar.toDataURL()} />
     </Center>
   );
+
   return (
     <Modalo
       open={<Box {...boxOptions}>{imageBox}</Box>}
       buttonProps={{ variant: "link" }}
       action="Open in Editor"
       modalBody={imageBox}
-      onClickAction={console.log}
+      onClickAction={() => {
+        setEditorLayers(layers);
+        router.push(Routes.Home());
+      }}
       modalProps={boxOptions}
     />
+    // <Designer initialLayersState={layers__Default} />
   );
 }
 
@@ -42,10 +55,11 @@ function Gallery({ ...props }) {
   // sizeOptions
   const [hasMore, setHasMore] = useState(true);
   const [items, setItems] = useState([]);
+  const manyMore = 10;
 
   const fetchMore = () => {
     setHasMore(items.length < 50);
-    setItems(items.concat(Array.from({ length: 2 })));
+    setItems(items.concat(Array.from({ length: manyMore })));
   };
 
   return (
