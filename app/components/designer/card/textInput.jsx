1785b244d84cb3796dda5e1b7585c7d9133d9f6c
Matias Agelvis <Magelvisdoz@gmail.com>
2022-10-27 14:55:09 -0400

    Add insertTextAtCursor on textInput
    Button and function to handle text insertion at cursor or selected area

--- app/components/designer/card/textInput.jsx
+++ app/components/designer/card/textInput.jsx
@@ -7,27 +7,69 @@ import {
   Highlight,
   Text,
   Box,
+  Button,
+  InputRightElement,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverAnchor,
+  Portal,
 } from "@chakra-ui/react";
 import { textLength } from "app/lib/avatara";
+import { useEffect, useRef } from "react";
 
 function TextInput({ text, setText, maxLength = 3 }) {
+  const inputRef = useRef(null);
+
+  function insertTextAtCursor(ref, text, insert) {
+    const curr = ref.current;
+    if (!("selectionStart" in curr) || !("selectionEnd" in curr)) {
+      return text;
+    }
+    const textBeforeCursorPosition = text.substring(0, curr.selectionStart);
+    const textAfterCursorPosition = text.substring(curr.selectionEnd);
+
+    return textBeforeCursorPosition + insert + textAfterCursorPosition;
+  }
+
+  let handleClick = (insert) =>
+    setText(insertTextAtCursor(inputRef, text, insert));
+
   return (
-    <InputGroup>
-      <Input
-        value={text}
-        onChange={(event) => setText(event.target.value)}
-        // maxLength={maxLength}
-        placeholder="Input your text"
-      />
-      <InputRightAddon
-        {...(textLength(text) > maxLength && { backgroundColor: "red.200" })}
-        children={
-          <Code background={"transparent"}>
-            {textLength(text)}/{maxLength}
-          </Code>
-        }
-      />
-    </InputGroup>
+    <Popover>
+      <InputGroup>
+        <Input
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+          // maxLength={maxLength}
+          placeholder="Input your text"
+          ref={inputRef}
+        />
+        <InputRightElement w="fit-content">
+          <PopoverTrigger>
+            <Button>Icons</Button>
+          </PopoverTrigger>
+          <Portal>
+            <PopoverContent>
+              <PopoverArrow />
+              <PopoverCloseButton />
+              <PopoverHeader>HI</PopoverHeader>
+              <PopoverBody>
+                <Button onClick={() => handleClick(":f123:")}>
+                  INSERT ':f123:'
+                </Button>
+              </PopoverBody>
+            </PopoverContent>
+          </Portal>
+        </InputRightElement>
+        {/*<InputRightAddon children={} />*/}
+      </InputGroup>
+    </Popover>
   );
 }
 
