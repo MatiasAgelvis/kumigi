798720ee919fc656a12631e2a1a764fabc1f6a77
Matias Agelvis <Magelvisdoz@gmail.com>
2022-11-06 12:47:34 -0400

    Use new AccordionMenu api

--- pages/gallery/index.tsx
+++ pages/gallery/index.tsx
@@ -1,22 +1,37 @@
 import Layout from "app/core/layouts/Layout";
 import { BlitzPage } from "@blitzjs/next";
 import Gallery from "app/components/gallery";
-import sizeComponents from "app/components/sizeComponents";
+import sizeComponents from "app/components/size/sizeComponents";
 import buttonOptions from "app/utils/buttonOptions";
 import AccordionMenu from "app/components/designer/accordionMenu/accordionMenu";
 import boxOptions from "app/utils/boxOptions";
+import getSimpleDesigns from "app/simple-designs/queries/getSimpleDesigns";
+import { useInfiniteQuery, useQuery } from "@blitzjs/rpc";
+import sizeFormatted from "app/components/size/sizeFormatted";
 
 const UserGallery: BlitzPage = () => {
+  const [
+    projectPages,
+    { isFetching, isFetchingNextPage, fetchNextPage, hasNextPage },
+  ] = useInfiniteQuery(
+    getSimpleDesigns,
+    (page = { take: 3, skip: 0 }) => page,
+    {
+      getNextPageParam: (lastPage) => lastPage.nextPage,
+    }
+  );
   return (
     <Layout title="Random Gallery">
       <main>
         <AccordionMenu
           name="Size Options"
-          options={sizeComponents()}
+          body={sizeFormatted()}
           buttonProps={buttonOptions}
           wrapperProps={{ px: boxOptions.p }}
         />
-        <Gallery />
+        <Gallery
+          layerGenerator={() => projectPages.map((x) => x.simpleDesigns)}
+        />
       </main>
     </Layout>
   );
