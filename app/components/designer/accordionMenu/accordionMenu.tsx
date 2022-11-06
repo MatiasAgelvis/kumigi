b1075cfbb40fc1fa8b9b772c54c27b11694f80d8
Matias Agelvis <Magelvisdoz@gmail.com>
2022-11-06 12:46:04 -0400

    Modify accordionMenu to accept an arbitrary body

--- app/components/designer/accordionMenu/accordionMenu.tsx
+++ app/components/designer/accordionMenu/accordionMenu.tsx
@@ -1,33 +1,21 @@
 import {
   Box,
-  VStack,
   useDisclosure,
   Button,
   Collapse,
   BoxProps,
   ButtonProps,
 } from "@chakra-ui/react";
-import boxOptions from "app/utils/boxOptions";
 import { ReactNode } from "react";
-import Item from "./item";
-
-export type Options = Array<{ name: string; input: ReactNode }>;
 
 type Props = {
   name: string;
-  options: Options;
-  spacing?: number;
+  body: ReactNode;
   wrapperProps?: BoxProps;
   buttonProps?: ButtonProps;
 };
 
-function AccordionMenu({
-  name,
-  options,
-  spacing = 4,
-  wrapperProps,
-  buttonProps,
-}: Props) {
+function AccordionMenu({ name, body, wrapperProps, buttonProps }: Props) {
   const { isOpen, onToggle } = useDisclosure();
 
   return (
@@ -36,13 +24,7 @@ function AccordionMenu({
         {isOpen ? "Hide" : "Show"} {name}
       </Button>
       <Collapse in={isOpen} animateOpacity>
-        <Box mt={spacing} {...boxOptions}>
-          <VStack spacing={spacing} align="stretch">
-            {Array.from(options).map((option, i) => (
-              <Item key={i} name={option.name} input={option.input} />
-            ))}
-          </VStack>
-        </Box>
+        {body}
       </Collapse>
     </Box>
   );
