728de2f7be6f438dd8e862ef9a4ef2a82732aeac
Matias Agelvis <Magelvisdoz@gmail.com>
2022-11-06 12:44:54 -0400

    Add RandomGalleryComponent

---
+++ app/components/gallery/random.tsx
@@ -0,0 +1,17 @@
+import Gallery from "app/components/gallery";
+import { idCard } from "app/utils/createLayer";
+import { randomLayers } from "app/lib/avatara";
+import { FC } from "react";
+import Modalo from "../modal";
+import sizeFormatted from "../size/sizeFormatted";
+
+const RandomGalleryComponent = () => {
+  return (
+    <>
+      <Modalo open={"Size Options"} modalBody={sizeFormatted()} />
+      <Gallery layerGenerator={() => randomLayers().map(idCard)} />
+    </>
+  );
+};
+
+export default RandomGalleryComponent;
