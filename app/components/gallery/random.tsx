b408dbd4e18e810371107099afa3a8998a9e69d6
Matias Agelvis <Magelvisdoz@gmail.com>
2022-11-07 13:28:26 -0400

    Add fetchMore function to gallery/random.tsx

--- app/components/gallery/random.tsx
+++ app/components/gallery/random.tsx
@@ -1,16 +1,46 @@
 import Gallery from "app/components/gallery";
-import { idCard } from "app/utils/createLayer";
 import { randomLayers } from "app/lib/avatara";
-import { FC } from "react";
+import { FC, ReactElement, ReactNode, useState } from "react";
 import Modalo from "../modal";
 import sizeFormatted from "../size/sizeFormatted";
+import Avatar from "./Avatar";
+import { idCard } from "app/utils/createLayer";
+import { v4 as uuidv4 } from "uuid";
+import { Box, VStack, Wrap } from "@chakra-ui/react";
+import boxOptions from "app/utils/boxOptions";
 
 const RandomGalleryComponent = () => {
+  // <Avatar layers={item} key={`avatar_${index}`} />
+  const [items, setItems] = useState<ReactNode[]>([]);
+  const [hasMore, setHasMore] = useState(true);
+  const manyMore = 10;
+  const limit = 100;
+
+  const layerGenerator = () => randomLayers().map(idCard);
+
+  const fetchMore = () => {
+    setHasMore(items.length < limit);
+    setItems(
+      items.concat(
+        Array.from({ length: manyMore }, () => (
+          <Avatar key={uuidv4()} layers={layerGenerator()} />
+        ))
+      )
+    );
+  };
+
   return (
-    <>
-      <Modalo open={"Size Options"} body={sizeFormatted()} />
-      <Gallery layerGenerator={() => randomLayers().map(idCard)} />
-    </>
+    <VStack w="full" spacing={8}>
+      <Wrap w="full" justify={"center"}>
+        <Modalo
+          open={"Size Options"}
+          body={sizeFormatted()}
+          modalProps={boxOptions}
+        />
+      </Wrap>
+
+      <Gallery items={items} fetchMore={fetchMore} hasMore={hasMore} w="full" />
+    </VStack>
   );
 };
 
