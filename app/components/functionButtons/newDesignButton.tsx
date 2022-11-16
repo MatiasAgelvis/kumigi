d29764416033b02f829110826fb28fab14fe2695
Matias Agelvis <Magelvisdoz@gmail.com>
2022-11-16 11:32:14 -0400

    Fix new design button not redirecting to editor

--- app/components/functionButtons/newDesignButton.tsx
+++ app/components/functionButtons/newDesignButton.tsx
@@ -1,26 +1,31 @@
-import { PlusSquareIcon } from "@chakra-ui/icons";
+import { AddIcon } from "@chakra-ui/icons";
 import { Button, ButtonProps } from "@chakra-ui/react";
 import { buttonSize } from "app/utils/buttonOptions";
 import { layers__Default } from "app/utils/createLayer";
 import { useRecoilState } from "recoil";
 import { layersAtom, nameAtom } from "app/utils/store";
 import { name__default } from "app/utils/name";
+import { useRouter } from "next/router";
+import { Routes } from "@blitzjs/next";
 
 export default function NewDesignButton({ ...props }: ButtonProps) {
   const [name, setName] = useRecoilState(nameAtom);
   const [layers, setLayers] = useRecoilState(layersAtom);
+  const router = useRouter();
 
   const size = buttonSize;
 
   return (
     <Button
       size={size}
-      leftIcon={<PlusSquareIcon />}
+      leftIcon={<AddIcon />}
       colorScheme="green"
       onClick={() => {
         setLayers(layers__Default);
         setName(name__default);
+        router.push(Routes.Home());
       }}
+      {...props}
     >
       New
     </Button>
