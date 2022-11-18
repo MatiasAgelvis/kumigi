a37bad1147684dd0e4a911ec8c3477d275e3a810
Matias Agelvis <Magelvisdoz@gmail.com>
2022-11-17 21:12:01 -0400

    Rename utils/sizeState.ts to hooks/useSizeState.ts

--- app/hooks/useSizeState.ts
+++ app/hooks/useSizeState.ts
@@ -1,11 +1,9 @@
-import { heightAtom, widthAtom } from "./store";
+import { heightAtom, widthAtom } from "app/utils/store";
 import { useRecoilState } from "recoil";
 
-export default function sizeState() {
-
+export default function useSizeState() {
   const [height, setHeight] = useRecoilState(heightAtom);
   const [width, setWidth] = useRecoilState(widthAtom);
 
-
-  return { height, setHeight, width, setWidth }
-};
+  return { height, setHeight, width, setWidth };
+}
