f4e003a38ac180af8f2ec49036e458eb3083bdb5
Matias Agelvis <Magelvisdoz@gmail.com>
2022-11-17 21:07:56 -0400

    Migrate responsiveLayouts.ts to TS

--- app/utils/responsiveLayouts.ts
+++ app/utils/responsiveLayouts.ts
@@ -1,6 +1,6 @@
 export const mobileOnly = ["block", "block", "none"];
 export const desktopOnly = ["none", "none", "block"];
-export const chakraViewportSplit = (mobile, desktop) => [
+export const chakraViewportSplit = <T>(mobile: T, desktop: T) => [
   mobile,
   mobile,
   desktop,
