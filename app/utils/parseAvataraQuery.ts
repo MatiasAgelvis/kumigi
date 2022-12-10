3b59dff3cc60d7cf3acb7f1cf0ef317378de1bec
Matias Agelvis <magelvisdoz@gmail.com>
2022-12-09 20:25:47 -0400

    Parse new api schema

--- app/utils/parseAvataraQuery.ts
+++ app/utils/parseAvataraQuery.ts
@@ -1,40 +1,78 @@
+import { randomLayer } from "app/lib/avatara"
 import {
   ApiQuery,
   AvataraQuery,
   Layer,
   LayerNoId,
   Shape,
-} from "app/types/avatara";
-import { parseArrayString, testHexColor } from "../utils/common";
-import { makeLayers } from "./makeLayers";
+} from "app/types/avatara"
+import Color from "color"
+import { parseArrayString, testHexColor } from "../utils/common"
+import { createLayer } from "./createLayer"
 
-export function parseAvataraQuery(
-  query: ApiQuery & AvataraQuery
-): [number, number, Layer[] | LayerNoId[]] {
-  const {
-    height: heightString = "200",
-    width: widthString = "200",
-    colors: colorsString = "[000]",
-    texts: textString = "",
-    fonts: fontsString = "plex",
-    shapes = [],
-  } = query;
+function passOrReplace<T>(
+  value: T,
+  test: (x: T) => boolean = Boolean,
+  alternative: T
+) {
+  return test(value) ? value : alternative
+}
 
+function parseColor(colorString: string | undefined, dft: string) {
+  if (!colorString) return dft
+  // if the color has the format hex(123456) return the hashed value
+  if (colorString.match(/hex\((.{0,6})\)/g)) {
+    return "#" + colorString.matchAll(/hex\((.{0,6})\)/g)[0][1]
+  }
+
+  const append = testHexColor(colorString) ? "#" : ""
+  return colorString ? Color(append + colorString).toString() : dft
+}
+
+function parseSizes(sizesString: string | undefined, dft: number): number[][] {
+  // default return
+  if (!sizesString) return [[dft, dft]]
+
+  const splitted = parseArrayString(sizesString).map((size) => size.split("x"))
+
+  const parsed = splitted.map((sizes) => sizes.map((size) => parseInt(size)))
+
+  const passCondition = (x) => Boolean(x) && !isNaN(x)
+  // duplicate single elements, all must be tuples, discard extra values
+  return parsed.map((size) =>
+    (size.length == 1 ? [size[0], size[0]] : [size[0], size[1]]).map(
+      // replace undefineds and NaNs with dft
+      (untested) => passOrReplace(untested, passCondition, dft)
+    )
+  )
+}
+
+function parseLayer(layerString: string, dft: LayerNoId): LayerNoId {
+  const parsed = parseArrayString(layerString)
+
+  if (!parsed || !parsed[0]) return dft
+
+  return {
+    shape: (parsed[0] || "empty") as Shape,
+    color: parseColor(parsed[1], "#000"),
+    font: parsed[2] || "pt",
+    text: parsed.slice(3).join(",") || "",
+    display: true,
+  }
+}
+
+export function parseAvataraQuery(query: ApiQuery & AvataraQuery): {
+  sizes: number[][]
+  layers: Layer[] | LayerNoId[]
+} {
   // parse the height and width
-  const height = parseInt(heightString);
-  const width = parseInt(widthString);
+  const sizes = parseSizes(query.sizes, 400)
 
-  // make array of colors form query
-  const colors = parseArrayString(
-    colorsString,
-    (x) => (testHexColor(x) ? "#" : "") + x.toLowerCase()
-  );
+  const layersString = query.shapes || ["empty,transparent"]
 
-  const texts = parseArrayString(textString);
-  const fonts = parseArrayString(fontsString, (x) => x.toLowerCase());
+  const layers: LayerNoId[] = layersString.map((layerAsString) =>
+    parseLayer(layerAsString, createLayer())
+  )
 
-  // if only one font is given it will be reused for all texts
-  const layers: LayerNoId[] = makeLayers(shapes, colors, texts, fonts);
-
-  return [height, width, layers];
+  return { sizes, layers }
 }
