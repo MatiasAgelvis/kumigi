fcddb1cc6fbe760359eb00885af2c37e03f91ba3
Matias Agelvis <magelvisdoz@gmail.com>
2022-12-18 09:44:56 +0100

    Adapt to new size state management

--- app/hooks/useSizeComponents.tsx
+++ app/hooks/useSizeComponents.tsx
@@ -1,18 +1,27 @@
-import RangeInput from "app/components/designer/editor/rangeInput";
-import useSizeState from "app/hooks/useSizeState";
-import { Options } from "app/components/formatting/fullWidth";
+import RangeInput from "app/components/designer/editor/rangeInput"
+import useSizeState from "app/hooks/useSizeState"
+import { Options } from "app/components/formatting/fullWidth"
 
-export default function useSizeComponents(): Options {
-  const { height, setHeight, width, setWidth } = useSizeState();
+export default function useSizeComponents(index: number): Options {
+  const { size, setSize } = useSizeState(index)
+  const [width, height] = size
+
+  function setHeight(height) {
+    return setSize([width, height])
+  }
+
+  function setWidth(width) {
+    return setSize([width, height])
+  }
 
   return [
     {
       name: "Height",
-      input: <RangeInput value={height} setValue={setHeight} max={2000} />,
+      input: <RangeInput value={height!} setValue={setHeight} max={2000} />,
     },
     {
       name: "Width",
-      input: <RangeInput value={width} setValue={setWidth} max={2000} />,
+      input: <RangeInput value={width!} setValue={setWidth} max={2000} />,
     },
-  ];
+  ]
 }
