c87a65e76a02182c6b48fdb7c60309cbbbb9390b
Matias Agelvis <magelvisdoz@gmail.com>
2022-12-19 23:14:46 +0100

    Add sizePill.tsx

---
+++ app/components/size/sizePill.tsx
@@ -0,0 +1,58 @@
+import { CloseIcon, EditIcon } from "@chakra-ui/icons"
+import {
+  Button,
+  ButtonGroup,
+  ButtonGroupProps,
+  ButtonProps,
+  IconButton,
+} from "@chakra-ui/react"
+import useSizeState from "app/hooks/useSizeState"
+import { currentSizeAtom } from "app/utils/store"
+import { useRecoilState } from "recoil"
+import Popovero from "../popover"
+import SizeFormatted from "./sizeFormatted"
+
+export default function SizePill({
+  index,
+  editButtonProps,
+  deleteButtonProps,
+  mainButtonProps,
+  ...props
+}: {
+  index: number
+  editButtonProps?: ButtonProps
+  deleteButtonProps?: ButtonProps
+  mainButtonProps?: ButtonProps
+} & ButtonGroupProps) {
+  const { size, setSize, deleteSize, length: sizeLength } = useSizeState(index)
+  const [width, height] = size
+  const [currentSize, setCurrentSize] = useRecoilState(currentSizeAtom)
+
+  return (
+    <ButtonGroup
+      isAttached
+      variant={currentSize == index ? "solid" : "outline"}
+      {...props}
+    >
+      <Popovero
+        triggerer={<IconButton icon={<EditIcon />} aria-label="Change size" />}
+        placement={"top"}
+        body={<SizeFormatted index={index} />}
+      />
+
+      <Button
+        onClick={() => {
+          setCurrentSize(index)
+        }}
+      >
+        {width}x{height}
+      </Button>
+      <IconButton
+        icon={<CloseIcon />}
+        aria-label="Delete size"
+        onClick={deleteSize}
+        disabled={sizeLength == 1}
+      />
+    </ButtonGroup>
+  )
+}
