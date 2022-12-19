fdc9c8fd64741328071b300420c3c8122f3dbfbf
Matias Agelvis <magelvisdoz@gmail.com>
2022-12-19 23:16:30 +0100

    Add multiple sizes feature

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,13 +1,26 @@
-import { Box, BoxProps, Flex, HStack, VStack } from "@chakra-ui/react";
-import APIurl from "./apiUrl";
-import boxOptions from "app/utils/boxOptions";
+import {
+  Box,
+  BoxProps,
+  Button,
+  Flex,
+  HStack,
+  IconButton,
+  VStack,
+} from "@chakra-ui/react"
+import APIurl from "./apiUrl"
+import boxOptions from "app/utils/boxOptions"
 
-import { NextComponentType } from "next";
-import buttonOptions from "app/utils/buttonOptions";
-import SizeFormatted from "../size/sizeFormatted";
-import Modalo from "../modal";
+import { NextComponentType } from "next"
+import buttonOptions from "app/utils/buttonOptions"
+import SizeFormatted from "../size/sizeFormatted"
+import Modalo from "../modal"
+import { useRecoilState, useRecoilValue } from "recoil"
+import { sizesAtom } from "app/utils/store"
+import SizePill from "../size/sizePill"
+import { AddIcon } from "@chakra-ui/icons"
 
 const Footer: NextComponentType = ({ ...props }: BoxProps) => {
+  const [sizes, setSizes] = useRecoilState(sizesAtom)
   return (
     <Box {...props}>
       <Box {...boxOptions} m="1rem">
@@ -17,11 +30,23 @@ const Footer: NextComponentType = ({ ...props }: BoxProps) => {
             layers={layers}
             parameters={{ height, width }}
           />*/}
-          <Modalo
+          {/*<Modalo
             open={"Size Options"}
             buttonProps={{ variant: "outline", colorScheme: "blue" }}
-            body={<SizeFormatted />}
+            body={<SizeFormatted index={0} />}
             modalProps={boxOptions}
+          />*/}
+          {sizes.map((size, index) => (
+            <SizePill
+              key={`size_${index}`}
+              index={index}
+              colorScheme={"teal"}
+            />
+          ))}
+          <IconButton
+            icon={<AddIcon />}
+            aria-label={"Add another Size"}
+            onClick={() => setSizes([...sizes, [400, 400]])}
           />
         </HStack>
       </Box>
@@ -32,7 +57,7 @@ const Footer: NextComponentType = ({ ...props }: BoxProps) => {
         </Heading>
       </Center>*/}
     </Box>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
