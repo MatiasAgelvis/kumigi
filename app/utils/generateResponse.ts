9c80880ee06177474f2e3de26590505db0db864d
Matias Agelvis <magelvisdoz@gmail.com>
2022-12-22 11:49:52 +0100

    Add option to render browser <Canvas/>

--- app/utils/generateResponse.ts
+++ app/utils/generateResponse.ts
@@ -2,27 +2,47 @@ import Avatara from "../../app/lib/avatara"
 import applyLayers from "../../app/utils/applyLayers"
 import JSZip from "jszip"
 import { Layer, LayerNoId } from "app/types/avatara"
+import saveAs from "file-saver"
+
+function render(avatar, browser = false) {
+  return browser ? dataURLtoBlob(avatar.toDataURL()) : avatar.toBuffer()
+}
+
+function dataURLtoBlob(dataurl) {
+  var arr = dataurl.split(","),
+    mime = arr[0].match(/:(.*?);/)[1],
+    bstr = atob(arr[1]),
+    n = bstr.length,
+    u8arr = new Uint8Array(n)
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n)
+  }
+  return new Blob([u8arr], { type: mime })
+}
 
 export default function generateResponse(
   layers: Layer[] | LayerNoId[],
-  sizes: number[][]
+  sizes: number[][],
+  browser = false
 ) {
   if (sizes.length == 1) {
     const [width, height] = sizes[0]!
     const avatar = new Avatara(width, height)
-    applyLayers(avatar, layers)
 
-    return avatar.toBuffer()
+    applyLayers(avatar, layers)
+    return render(avatar, browser)
   } else {
     const zip = new JSZip()
 
     sizes.forEach(([width, height]) => {
       const avatar = new Avatara(width, height)
       applyLayers(avatar, layers)
-      // console.log(width, height)
-      zip.file(`avatara/${width}x${height}` + ".png", avatar.toBuffer())
+      const rendered = render(avatar, browser)
+      zip.file(`avatara/${width}x${height}` + ".png", render(avatar, browser))
     })
 
-    return zip.generateNodeStream({ type: "nodebuffer", streamFiles: true })
+    return browser
+      ? zip.generateAsync({ type: "blob" })
+      : zip.generateNodeStream({ type: "nodebuffer", streamFiles: true })
   }
 }
