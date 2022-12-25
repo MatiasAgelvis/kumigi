95a29200025ef0c4a0ce63c764b76134c94d664e
Matias Agelvis <magelvisdoz@gmail.com>
2022-12-25 22:23:53 +0100

    Add SetStateType  to avatara.ts

--- app/types/avatara.ts
+++ app/types/avatara.ts
@@ -1,46 +1,50 @@
-import Avatara from "app/lib/avatara";
-import { DefinedShapes } from "app/lib/shapes";
-import { NextApiRequest } from "next";
-import { Actions, State } from "use-undo";
+import Avatara from "app/lib/avatara"
+import { DefinedShapes } from "app/lib/shapes"
+import { NextApiRequest } from "next"
+import { Dispatch } from "react"
+import { SetterOrUpdater } from "recoil"
+import { Actions, State } from "use-undo"
 
-export type OKey = string | number | symbol;
+export type OKey = string | number | symbol
 
-export type Shape = DefinedShapes | "text" | "random";
+export type Shape = DefinedShapes | "text" | "random"
 
 export type LayerNoId = {
-  shape: Shape;
-  color: string;
-  text: string;
-  font: string;
-  display: boolean;
-  id?: string;
-  [x: string | number | symbol]: unknown;
-};
+  shape: Shape
+  color: string
+  text: string
+  font: string
+  display: boolean
+  id?: string
+  [x: string | number | symbol]: unknown
+}
 
 export type Layer = {
-  shape: Shape;
-  color: string;
-  text: string;
-  font: string;
-  display: boolean;
-  id: string;
-  [x: string | number | symbol]: unknown;
-};
+  shape: Shape
+  color: string
+  text: string
+  font: string
+  display: boolean
+  id: string
+  [x: string | number | symbol]: unknown
+}
 
 export type AvataraQuery = Partial<{
-  height: string;
-  width: string;
-  sizes: string;
-  colors: string;
-  texts: string;
-  fonts: string;
-  shapes: Shape[];
-}>;
+  height: string
+  width: string
+  sizes: string
+  colors: string
+  texts: string
+  fonts: string
+  shapes: Shape[]
+}>
 
-export type ApiQuery = NextApiRequest["query"];
+export type ApiQuery = NextApiRequest["query"]
 
-export type UseUndoType<T> = [State<T>, Actions<T>];
+export type UseUndoType<T> = [State<T>, Actions<T>]
 
 export interface NotNestedObject {
-  [x: string]: number | boolean | string | null | undefined;
+  [x: string]: number | boolean | string | null | undefined
 }
+
+export type SetStateType<T> = [T, Dispatch<T> | SetterOrUpdater<T>]
