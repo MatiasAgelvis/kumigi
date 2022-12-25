43c25d2367cd29c2b8fedc37151c4ee937a4c75b
Matias Agelvis <magelvisdoz@gmail.com>
2022-12-25 21:40:03 +0100

    Add sizes to the editor state in editButton.tsx

--- app/components/functionButtons/editButton.tsx
+++ app/components/functionButtons/editButton.tsx
@@ -1,51 +1,50 @@
-import { Routes } from "@blitzjs/next";
-import { Button, ButtonProps, Center } from "@chakra-ui/react";
-import { ReactNode, useState } from "react";
+import { Routes } from "@blitzjs/next"
+import { Button, ButtonProps, Center } from "@chakra-ui/react"
+import { ReactNode, useState } from "react"
 
-import Avatara from "app/lib/avatara";
-import applyLayers from "app/utils/applyLayers";
-import { useRecoilState } from "recoil";
-import { layersAtom, nameAtom } from "app/utils/store";
-import { useRouter } from "next/router";
-import { Layer } from "app/types/avatara";
-import { EditIcon } from "@chakra-ui/icons";
-import { name__default } from "app/utils/name";
-import useSizeState from "app/hooks/useSizeState";
+import Avatara from "app/lib/avatara"
+import applyLayers from "app/utils/applyLayers"
+import { useRecoilState } from "recoil"
+import { layersAtom, nameAtom, sizesAtom } from "app/utils/store"
+import { useRouter } from "next/router"
+import { Layer } from "app/types/avatara"
+import { EditIcon } from "@chakra-ui/icons"
+import { name__default } from "app/utils/name"
+import useSizeState from "app/hooks/useSizeState"
 
 export default function EditButton({
   layers: startLayers,
   name = name__default,
-  dimensions,
-
+  sizes,
   ...props
 }: {
-  layers: Layer[];
-  name: string;
-  dimensions: { height: number; width: number };
+  layers: Layer[]
+  name: string
+  sizes: number[][]
 } & ButtonProps) {
-  const { height, width } = dimensions;
-  const avatar = new Avatara(width, height);
-  const [layers] = useState(startLayers);
-  applyLayers(avatar, layers);
-  const [editorLayers, setEditorLayers] = useRecoilState(layersAtom);
-  const [nameState, setNameState] = useRecoilState(nameAtom);
-  const { setHeight, setWidth } = useSizeState();
-  const router = useRouter();
+  const [width, height] = sizes[0]!
+  const avatar = new Avatara(width, height)
+  const [layers] = useState(startLayers)
+  applyLayers(avatar, layers)
+  const [editorLayers, setEditorLayers] = useRecoilState(layersAtom)
+  const [nameState, setNameState] = useRecoilState(nameAtom)
+  // const { setSize } = useSizeState(0)
+  const [sizesRecoil, setSizes] = useRecoilState(sizesAtom)
+  const router = useRouter()
 
   return (
     <Button
       colorScheme={"teal"}
       onClick={() => {
-        setEditorLayers(layers);
-        setNameState(name);
-        setHeight(dimensions.height);
-        setWidth(dimensions.width);
-        router.push(Routes.Home());
+        setEditorLayers(layers)
+        setNameState(name)
+        setSizes(sizes)
+        router.push(Routes.Home())
       }}
       leftIcon={<EditIcon />}
       {...props}
     >
       Edit
     </Button>
-  );
+  )
 }
