1091192400aa09b0144f931a77f4b9af440e73d7
Matias Agelvis <magelvisdoz@gmail.com>
2022-12-26 17:08:48 +0100

    Fix zod type

--- app/simple-designs/mutations/deleteSimpleDesign.ts
+++ app/simple-designs/mutations/deleteSimpleDesign.ts
@@ -1,18 +1,18 @@
-import { resolver } from "@blitzjs/rpc";
-import db from "db";
-import { z } from "zod";
+import { resolver } from "@blitzjs/rpc"
+import db from "db"
+import { z } from "zod"
 
 const DeleteSimpleDesign = z.object({
-  id: z.string(),
-});
+  id: z.number(),
+})
 
 export default resolver.pipe(
   resolver.zod(DeleteSimpleDesign),
   resolver.authorize(),
   async ({ id }) => {
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-    const simpleDesign = await db.simpleDesigns.deleteMany({ where: { id } });
+    const simpleDesign = await db.simpleDesigns.deleteMany({ where: { id } })
 
-    return simpleDesign;
+    return simpleDesign
   }
-);
+)
