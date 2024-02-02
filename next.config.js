b189641520f203e4a6c2bae41d76d5838b8362af
Matias Agelvis <magelvisdoz@gmail.com>
2024-02-02 19:43:22 +0100

    Fix Canvas deploy on Vercel

--- next.config.js
+++ next.config.js
@@ -8,3 +8,12 @@ if (
     process.env.PWD
   }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
 }
+
+// next.config.js
+
+module.exports = {
+  // No need for next/babel unless specific transformations are needed
+  // experimental: {
+  //   appDir: true, // For custom directory structure (optional)
+  // },
+};
